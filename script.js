a680aae4bc95c2c2ee216b8a41268065da430fac
Yuze-co <yuraiye1@gmail.com>
2026-07-23 01:14:14 +0200

    Add mouse tracking glow effect to cursor

--- script.js
+++ script.js
@@ -180,3 +180,16 @@ tsParticles.load("particles-js",{
     }
 
 });
+// ==========================
+// Luz que sigue al ratón
+// ==========================
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+    glow.style.left = e.clientX + "px";
+
+    glow.style.top = e.clientY + "px";
+
+});
